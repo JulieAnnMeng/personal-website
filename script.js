2ff6b42e863ad7e4684a50bfb8490e1468f578aa
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-07-29 15:35:10 -0400

    setup script.js, setup tabs to open each tab while hiding others

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+    document.getElementById('home').style.display = "block";
+    document.getElementById('homeTab').className += " active";
+}
+
+function openTab(event, tabName) {
+    var i, tabContent, tab;
+    
+    tab = document.getElementsByClassName("tab");
+    for (i = 0; i < tab.length; i++) {
+      tab[i].className = tab[i].className.replace(" active", "");
+    }
+
+    tabContent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+  }
